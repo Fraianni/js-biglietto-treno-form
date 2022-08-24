63b525e37e312a44ad957579db99cbd88294439f
Fraianni <francescoianni1995@gmail.com>
2022-08-24 15:35:51 +0200

    corretta interazione select tag con javascript

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const passenger_name = document.getElementById('complete_name');
+const genera_button = document.getElementById('genera');
+const tot_km = document.getElementById('trip_km');
+const selcted_age = document.getElementById('options');
+const costo_per_km = 0.21;
+
+
+
+genera_button.addEventListener('click',
+    function () {
+        const km = tot_km.value;
+        const age = selcted_age.value;
+        let costo_tot = costo_per_km * km;
+
+        alert(age);
+
+        if (age == 'first') {
+            costo_tot = costo_tot - (costo_tot * 0.2);
+        }
+
+        else if (age == 'second') {
+            costo_tot = costo_tot - (costo_tot * 0.4);
+        }
+
+        alert("il costo totale per la persona di età " + age + " che percorre " + km + "km è di " + costo_tot.toFixed(2) + "euro");
+
+    }
+);
+
+
