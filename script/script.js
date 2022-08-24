2f24c8816123d2a76ea531c8c5af7e8ed348d357
Fraianni <francescoianni1995@gmail.com>
2022-08-24 16:34:37 +0200

    aggiunta biglietto più cod javascript

--- script/script.js
+++ script/script.js
@@ -6,25 +6,52 @@ const costo_per_km = 0.21;
 
 
 
+
+
 genera_button.addEventListener('click',
     function () {
         const km = tot_km.value;
         const age = selcted_age.value;
+        const name = passenger_name.value;
         let costo_tot = costo_per_km * km;
 
-        alert(age);
+        const generated_promo = document.querySelector('.offerta');
 
-        if (age == 'minorenne') {
-            costo_tot = costo_tot - (costo_tot * 0.2);
+        if (isNaN(km)) {
+            alert('Inserire numericamente i km')
+        }
+        else {
+            if (age == 'minorenne') {
+                costo_tot = costo_tot - (costo_tot * 0.2);
+                generated_promo.innerHTML += 'Tariffa ridotta del 20%';
+            }
+
+            else if (age == 'over') {
+                costo_tot = costo_tot - (costo_tot * 0.4);
+                generated_promo.innerHTML += 'Tariffa ridotta del 40%';
+
+            }
+
+            else {
+                generated_promo.innerHTML += 'Tariffa standard';
+
+            }
+
+            const generated_name = document.querySelector('.pass_name');
+            generated_name.innerHTML += name;
+
+            const num_carrozza = document.querySelector('.numero_carrozza');
+            num_carrozza.innerHTML += Math.floor((Math.random() * 9) + 1)
+
+            alert("il costo totale per " + name + costo_tot.toFixed(2) + "euro");
         }
 
-        else if (age == 'over') {
-            costo_tot = costo_tot - (costo_tot * 0.4);
-        }
 
-        alert("il costo totale per la persona di età " + age + " che percorre " + km + "km è di " + costo_tot.toFixed(2) + "euro");
 
     }
 );
+
+
+
 
 
